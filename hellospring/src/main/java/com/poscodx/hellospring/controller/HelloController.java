85eb8513b37b21db3d75d3a41af45ca24b958019
6loss0m <42835988+6loss0m@users.noreply.github.com>
2023-09-14 13:43:21 +0900

    [ADD] spring-practices git init

---
+++ hellospring/src/main/java/com/poscodx/hellospring/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.poscodx.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+		
+//	@RequestMapping("/hellospring/hello")
+	@RequestMapping("/hello")
+	public String hello() {
+		return "/WEB-INF/views/hello.jsp";
+	}
+
+}
