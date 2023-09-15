9b8910732ca95412efb6c1477b14095ab9954265
6loss0m <42835988+6loss0m@users.noreply.github.com>
2023-09-15 09:51:00 +0900

    @RequestMapping #1

--- hellospring/src/main/java/com/poscodx/hellospring/controller/HelloController.java
+++ hellospring/src/main/java/com/poscodx/hellospring/controller/HelloController.java
@@ -1,8 +1,10 @@
 package com.poscodx.hellospring.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
@@ -11,6 +13,37 @@ public class HelloController {
 	@RequestMapping("/hello")
 	public String hello() {
 		return "/WEB-INF/views/hello.jsp";
+	}
+	
+	@RequestMapping("/hello2")
+	public String hello2(String name) {
+		System.out.println(name);
+		return "/WEB-INF/views/hello.jsp";
+	}
+	
+	@RequestMapping("/hello3")
+	public ModelAndView hello3(String name) {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("/WEB-INF/views/hello3.jsp");
+		mav.addObject("name", name);
+		return mav;
+	}
+	
+	@RequestMapping("/hello4")
+	public String hello4(String name, Model model) {
+		model.addAttribute("name", name);
+		return "/WEB-INF/views/hello3.jsp";
+	}
+	
+	@ResponseBody
+	@RequestMapping("/hello5")
+	public String hello5() {
+		return "<h1>hello spring</h1>";
+	}
+	
+	@RequestMapping("/hello6")
+	public String hello6() {
+		return "rdirect:/hello";	//내가한 URL Mapping 만 사용
 	}
 
 }
